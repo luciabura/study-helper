6ec52913fa043ae6b888ba879a2d51cf3c862e88
Unknown <luciabura@gmail.com>
2017-12-10 22:14:29 +0000

    Updated unit tests for keyword extraction functions

--- unit_tests/test_unit_keywords.py
+++ unit_tests/test_unit_keywords.py
@@ -1,4 +1,7 @@
 from keyword_extraction import keywords
+from keyword_extraction import keywords_TR_lem
+
+from preprocessing.preprocessing import NLP
 
 
 def test_build_lemmas():
@@ -21,3 +24,34 @@ def test_build_lemmas():
 
     assert sorted(expected_l2w) == sorted(lemmas_to_words)
     assert sorted(expected_w2l) == sorted(words_to_lemmas)
+
+
+def test_get_graph_tokens():
+    text = u'This is a test sentence.'
+    # No graph words
+    INCLUDE_POS_FILTER = []
+    tokens = [token for token in NLP(text)]
+
+    graph_tokens = keywords_TR_lem.get_graph_tokens(tokens, INCLUDE_POS_FILTER)
+    expected_graph_tokens = []
+
+    assert graph_tokens == expected_graph_tokens
+
+    # Only nouns
+    INCLUDE_POS_FILTER = ['NN']
+    graph_tokens = keywords_TR_lem.get_graph_tokens(tokens, INCLUDE_POS_FILTER)
+    expected_graph_tokens = [tokens[3], tokens[4]]
+
+    assert graph_tokens == expected_graph_tokens
+
+
+def test_add_graph_edges():
+    # TODO:
+    return True
+
+
+def test_build_graph():
+    # TODO:
+    return True
+
+
