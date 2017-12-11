131403241b0abb2290cb90b3a9651a7c9d4f6d65
Lucia Bura <luciabura@gmail.com>
2017-12-11 10:15:03 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,16 +4,24 @@ PYTHON=`which python`
 
 ## Install pip in case it wasn't installed 
 echo "Installing pip"
-sudo apt get install pip 
-pip install --upgrade pip
+sudo apt-get install python-pip 
+sudo pip install --upgrade pip
 
 
-## Install dependencies
-echo "Instaling dependencies"
-pip install -r requirements.txt
+## Install dependencies for main part
+echo "Instaling main dependencies"
+sudo pip install -r requirements.txt
 sudo $PYTHON setup.py develop
 
 
 ## Download nltk data 
 sudo $PYTHON -c "import nltk; nltk.download('stopwords');nltk.download('punkt');nltk.download('wordnet'); nltk.download('averaged_perceptron_tagger')"
 sudo $PYTHON -m spacy download en
+
+
+## Install dependencies for evaluation
+echo "Instaling evaluation dependencies"
+sudo pip install summa 
+sudo pip install scipy 
+
+echo "Completed!"
