5c02f439966ad61898713d910a6e870d5c6b2cd1
Unknown <luciabura@gmail.com>
2017-12-29 12:55:41 +0000

    Change keyphrase evaluation metrics

--- evaluation/keywords_evaluation.py
+++ evaluation/keywords_evaluation.py
@@ -9,7 +9,77 @@ from preprocessing.preprocessing import clean_and_tokenize
 from gensim.summarization import keywords as gensim_keywords
 
 
+def keyphrase_f1(keys, ref_keys, alpha=0.5):
+    """
+    Given a set of reference values and a set of test values, return
+    the f-measure of the test values, when compared against the
+    reference values.  The f-measure is the harmonic mean of the
+    ``precision`` and ``recall``, weighted by ``alpha``.  In particular,
+    given the precision *p* and recall *r* defined by:
+
+    The f-measure is:
+
+    - *1/(alpha/p + (1-alpha)/r)*
+
+    :param keys: Set of values to compare against reference values
+    :param ref_keys: Reference values
+    :param alpha: weight
+    :return:
+    """
+
+    p = keyphrase_precision(keys, ref_keys)
+    r = keyphrase_recall(keys, ref_keys)
+    if p is 0 or r is 0:
+        return 0
+
+    return 1.0 / (alpha / p + (1 - alpha) / r)
+
+
+def keyphrase_precision(keys, ref_keys):
+    score = 0
+    for key in keys:
+        if key in ref_keys:
+            score += 1
+        else:
+            max_score = 0
+            for ref_key in ref_keys:
+                sc = precision(set(ref_key.split()), set(key.split()))
+                if sc > max_score:
+                    max_score = sc
+            score += max_score
+
+    if len(keys) == 0:
+        return 0
+
+    return score / len(keys)
+
+
+def keyphrase_recall(keys, ref_keys):
+    score = 0
+    for ref_key in ref_keys:
+        if ref_key in keys:
+            score += 1
+        else:
+            max_score = 0
+            for key in keys:
+                sc = recall(set(ref_key.split()), set(key.split()))
+                if sc > max_score:
+                    max_score = sc
+            score += max_score
+
+    if len(ref_keys) == 0:
+        return 0
+
+    return score / len(ref_keys)
+
+
 def keyword_score(extracted_keywords, reference_keywords):
+    if extracted_keywords is None or reference_keywords is None:
+        return None
+
+    if len(extracted_keywords) == 0 or len(reference_keywords) == 0:
+        return 0, 0, 0
+
     extracted_keywords_set = set(extracted_keywords)
     reference_keywords_set = set(reference_keywords)
 
@@ -17,26 +87,16 @@ def keyword_score(extracted_keywords, reference_keywords):
     prec = precision(reference_keywords_set, extracted_keywords_set)
     rec = recall(reference_keywords_set, extracted_keywords_set)
 
-    f_mes = format(f_mes, '.4f')
-    prec = format(prec, '.4f')
-    rec = format(rec, '.4f')
-
-    return f_mes, rec, prec
+    return f_mes, prec, rec
 
 
 def keyphrase_score(extracted_keyphrases, reference_keyphrases):
-    extracted_keyphrase_set = set(extracted_keyphrases)
-    reference_keyphrase_set = set(reference_keyphrases)
+    f_mes = keyphrase_f1(extracted_keyphrases, reference_keyphrases)
+    prec = keyphrase_precision(extracted_keyphrases, reference_keyphrases)
+    rec = keyphrase_recall(extracted_keyphrases, reference_keyphrases)
 
-    f_mes = f_measure(reference_keyphrase_set, extracted_keyphrase_set)
-    prec = precision(reference_keyphrase_set, extracted_keyphrase_set)
-    rec = recall(reference_keyphrase_set, extracted_keyphrase_set)
+    return f_mes, prec, rec
 
-    f_mes = format(f_mes, '.4f')
-    prec = format(prec, '.4f')
-    rec = format(rec, '.4f')
-
-    return f_mes, rec, prec
 
 def get_reference_keywords(reference_text):
     text_tokens = clean_and_tokenize(reference_text)
@@ -52,6 +112,18 @@ def get_reference_ngrams(reference_text):
     return ngrams
 
 
+def format_result(scores):
+    f1, precision, recall = scores
+
+    f1 = format(f1, '.4f')
+    precision = format(precision, '.4f')
+    recall = format(recall, '.4f')
+
+    result = ("F1: {}, Precision: {}, Recall: {}".format(
+        f1, precision, recall, ).replace(", ", "\n"))
+    return result
+
+
 if __name__ == '__main__':
     FILE_PATH = input('Enter a file path to get keywords for: ')
     REFERENCE_PATH = input('Enter a file path for reference keywords: ')
@@ -65,7 +137,7 @@ if __name__ == '__main__':
     graph_keyphrases = get_graph_keywords(FILE_TEXT)
     keyphrases_TR = get_graph_keywords_2(FILE_TEXT)
     keyphrases_TR_lem = get_graph_keywords_3(FILE_TEXT)
-    keyphrases_filtered = get_graph_keywords_4(FILE_TEXT)
+    keyphrases_filtered = get_graph_keywords_4(FILE_TEXT, filter=True)
 
     # Get the baselines keywords
     baseline_keywords = get_baseline_keywords(FILE_TEXT, len(REFERENCE_KEYWORDS))
@@ -74,23 +146,23 @@ if __name__ == '__main__':
     # Print out the scores
     print('\nMy TR implementation with lemmas:')
     keywords = list(set(' '.join(keyphrases_TR_lem).split()))
-    print(('Per-word:' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
-    print(('Per-keyphrase:' + str(keyphrase_score(keyphrases_TR_lem, REFERENCE_NGRAMS))))
+    print(('Per-word: \n' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
+    print(('Per-keyphrase: \n' + str(keyphrase_score(keyphrases_TR_lem, REFERENCE_NGRAMS))))
 
     print('\nMy TR implementation w/o lemmas:')
     keywords = list(set(' '.join(keyphrases_TR).split()))
-    print(('Per-word:' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
-    print(('Per-keyphrase:' + str(keyphrase_score(keyphrases_TR, REFERENCE_NGRAMS))))
+    print(('Per-word: \n' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
+    print(('Per-keyphrase: \n' + str(keyphrase_score(keyphrases_TR, REFERENCE_NGRAMS))))
 
     print('\nMy TR implementation w/o lemmas AND filtered:')
     keywords = list(set(' '.join(keyphrases_filtered).split()))
-    print(('Per-word:' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
-    print(('Per-keyphrase:' + str(keyphrase_score(keyphrases_filtered, REFERENCE_NGRAMS))))
+    print(('Per-word: \n' + str(keyword_score(keywords, REFERENCE_KEYWORDS))))
+    print(('Per-keyphrase: \n' + str(keyphrase_score(keyphrases_filtered, REFERENCE_NGRAMS))))
 
     print('\nEstablished implementation:')
     keywords = list(set((' '.join(established_keyphrases)).split()))
-    print('Per-word:' + str(keyword_score(keywords, REFERENCE_KEYWORDS)))
-    print('Per-keyphrase:' + str(keyphrase_score(established_keyphrases, REFERENCE_NGRAMS)))
+    print('Per-word: \n' + str(keyword_score(keywords, REFERENCE_KEYWORDS)))
+    print('Per-keyphrase: \n' + str(keyphrase_score(established_keyphrases, REFERENCE_NGRAMS)))
 
     print('\nBaseline implementation:')
     print((keyword_score(baseline_keywords, REFERENCE_KEYWORDS)))
