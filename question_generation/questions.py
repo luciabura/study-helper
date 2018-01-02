ec8b3632f301d27c337dad62dda35020e0ec6ed5
Unknown <luciabura@gmail.com>
2018-01-02 11:43:59 +0000

    Added more patterns

--- question_generation/questions.py
+++ question_generation/questions.py
@@ -15,12 +15,20 @@ INFINITY = math.inf
 
 ATTR = Matcher(NLP.vocab)
 DOBJ = Matcher(NLP.vocab)
+AGENT = Matcher(NLP.vocab)
+POBJ = Matcher(NLP.vocab)
+
+MATCHER = Matcher(NLP.vocab)
 
 OP = 'OP'
+ANY = {IS_ALPHA: True, OP: "*"}
+
 WHO_ENTS = ['PERSON', 'NORP']
 WHEN_ENTS = ['DATE']
 WHERE_ENTS = ['LOCATION', 'FACILITY', 'ORG', 'LOC', 'GPE']
-WHERE_PREPS = ['to', 'on', 'at', 'over', 'in', 'behind', 'above', 'below', 'from']
+HOW_MUCH_ENTS = ['MONEY', 'PERCENT']
+WHEN_PREPS = ['before', 'after', 'since', 'until']
+WHERE_PREPS = ['to', 'on', 'at', 'over', 'in', 'behind', 'above', 'below', 'from', 'inside', 'outside']
 
 
 class Match:
@@ -39,6 +47,9 @@ class Match:
     def get_sentence(self):
         return self.sentence
 
+    def get_tokens_by_dependency(self, dependency):
+        return [tok for tok in self.span if tok.dep_ == dependency]
+
 
 def choose_wh_word(span):
     """
@@ -48,31 +59,50 @@ def choose_wh_word(span):
         Ann: subj -> who?
     """
     wh_word = 'What'
+    # Choose 'how much'
+    if any(tok.ent_type_ in HOW_MUCH_ENTS for tok in span):
+        return 'How much'
 
     # Choose 'who'
     if any(tok.ent_type_ in WHO_ENTS for tok in span) or has_pronouns(span):
         return 'Who'
 
+    # Choose 'when'
+    if any(tok.ent_type_ in WHEN_ENTS or tok.text in WHEN_PREPS for tok in span):
+        return 'When'
+
     # Choose 'where'
     for tok in span:
-        if tok.ent_type_ in WHERE_ENTS and tok.head.dep_ == 'prep' and tok.head in WHERE_PREPS:
+        if tok.ent_type_ in WHERE_ENTS and tok.head.dep_ == 'prep' and tok.head.text in WHERE_PREPS:
             return 'Where'
-
-    # Choose 'when'
-    if any(tok.ent_type_ in WHEN_ENTS for tok in span):
-        return 'When'
 
     return wh_word
 
 
 def initialize_patterns():
-    attribute = [{DEP: 'nsubj'}, {POS: 'VERB'}, {IS_ALPHA: True, OP: "*"}, {DEP: 'attr'}]
-    ATTR.add("Attribute", None, attribute)
+    # This will match wrongly on is created, was made etc
+    attribute_1 = [{DEP: 'nsubj'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'attr'}]
+    attribute_2 = [{DEP: 'nsubjpass'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'attr'}]
 
-    direct_object = [{DEP: 'nsubj'}, {POS: 'VERB'}, {IS_ALPHA: True, OP: "*"}, {DEP: 'dobj'}]
-    DOBJ.add("Direct object", None, direct_object)
+    direct_object_1 = [{DEP: 'nsubj'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'dobj'}]
+    direct_object_2 = [{DEP: 'nsubjpass'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'dobj'}]
 
-    
+    prep_object_1 = [{DEP: 'nsubj'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'prep'},
+                     ANY, {DEP: 'pobj'}]
+    prep_object_2 = [{DEP: 'nsubjpass'}, ANY, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'prep'},
+                     ANY, {DEP: 'pobj'}]
+
+    agent_1 = [{DEP: 'nsubj'}, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'agent'}, ANY, {DEP: 'pobj'}]
+    agent_2 = [{DEP: 'nsubjpass'}, {POS: 'VERB', DEP: 'ROOT'}, ANY, {DEP: 'agent'}, ANY, {DEP: 'pobj'}]
+
+    MATCHER.add("ATTR", None, attribute_1)
+    MATCHER.add("ATTR", None, attribute_2)
+    MATCHER.add("DOBJ", None, direct_object_1)
+    MATCHER.add("DOBJ", None, direct_object_2)
+    MATCHER.add("POBJ", None, prep_object_1)
+    MATCHER.add("POBJ", None, prep_object_2)
+    MATCHER.add("AGENT", None, agent_1)
+    MATCHER.add("AGENT", None, agent_2)
 
 
 def sort_scores(scores):
@@ -89,12 +119,24 @@ def prepare_question_verb(verb, sentence, includes_subject):
     # Have to check verb for more than 1 word => needn't prepare question verb
     # eg: can take, will see, etc
     vp = get_verb_phrase(verb, sentence)
+    q_verb = []
 
     if len(vp) > 1:
         # This means we already have a composite verb
-        return [tok.text for tok in vp]
+        q_verb = []
+        # Should find better way to treat special cases
+        if not includes_subject and (vp[0].text == 'am' or vp[0].text == 'are'):
+            q_verb.append('is')
+        elif not includes_subject and vp[0].text == 'were':
+            q_verb.append('was')
+        elif not includes_subject and vp[0].text == 'have':
+            q_verb.append('has')
+        else:
+            q_verb.append(vp[0].text)
 
-    q_verb = []
+        q_verb.extend([tok.text for tok in vp[1:]])
+        return q_verb
+
     if is_past_tense(verb):
         if includes_subject and verb is not 'was':
             # Who did Harry meet? from Harry met Sally.
@@ -107,7 +149,6 @@ def prepare_question_verb(verb, sentence, includes_subject):
     else:
         if is_3rd_person(verb):
             if includes_subject and verb.text != 'is':
-                print(verb)
                 # Where does Harry go? from Harry goes to the store.
                 q_verb.append('does')
                 q_verb.append(verb.lemma_)
@@ -137,6 +178,132 @@ def format_phrase(span):
     return ' '.join(phrase)
 
 
+def generate_prepositional_questions(match):
+    questions = []
+    sentence = match.get_sentence()
+    subject = match.get_first_token()
+    pobject = match.get_last_token()
+    verb = subject.head
+    preposition = pobject.head
+
+    # Add checks for wrong matches
+    if preposition.head != verb:
+        return
+
+    subject_phrase = extract_noun_phrase(subject, sentence)
+    pobject_phrase = extract_noun_phrase(preposition, sentence)
+
+    wh_word_subject = choose_wh_word(subject_phrase)
+    wh_word_pobject = choose_wh_word(pobject_phrase)
+
+    verb_form_with_subject = prepare_question_verb(verb, sentence, includes_subject=True)
+    verb_form_with_pobject = prepare_question_verb(verb, sentence, includes_subject=False)
+
+    if is_valid_subject(subject_phrase):
+        question = [wh_word_pobject]
+
+        if len(verb_form_with_subject) == 1:
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+        else:
+            # I don't like this ... could have more than 2 in verb phrase, not in attribute, but other ones yes
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+            question.extend(verb_form_with_subject[1:])
+
+        # Very bad fix
+        if wh_word_pobject == 'What':
+            question.append(preposition.text)
+
+        question = ' '.join(question)
+
+        question += '?'
+        questions.append(question)
+
+    question = [wh_word_subject]
+    question.extend(verb_form_with_pobject)
+    question.append(format_phrase(pobject_phrase))
+
+    question = ' '.join(question)
+    question += '?'
+    questions.append(question)
+
+    return questions
+
+
+def generate_agent_questions(match):
+    """Only matches the by agent preposition"""
+    questions = []
+    sentence = match.get_sentence()
+    subject = match.get_first_token()
+    pobject = match.get_last_token()
+    verb = subject.head
+
+    subject_phrase = extract_noun_phrase(subject, sentence)
+    pobject_phrase = extract_noun_phrase(pobject, sentence)
+
+    wh_word_subject = choose_wh_word(subject_phrase)
+    wh_word_pobject = choose_wh_word(pobject_phrase)
+
+    verb_form_with_subject = prepare_question_verb(verb, sentence, includes_subject=True) + ['by']
+    verb_form_with_pobject = prepare_question_verb(verb, sentence, includes_subject=False) + ['by']
+
+    if is_valid_subject(subject_phrase):
+        question = [wh_word_pobject]
+
+        if len(verb_form_with_subject) == 1:
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+        else:
+            # I don't like this ... could have more than 2 in verb phrase, not in attribute, but other ones yes
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+            question.extend(verb_form_with_subject[1:])
+
+        question = ' '.join(question)
+        question += '?'
+        questions.append(question)
+
+    question = [wh_word_subject]
+    question.extend(verb_form_with_pobject)
+    question.append(format_phrase(pobject_phrase))
+
+    question = ' '.join(question)
+    question += '?'
+    questions.append(question)
+
+    return questions
+
+
+# def make_questions(sentence, wh_word, subj, verb, obj, preposition=None):
+#     questions = []
+#     verb_form_with_subject = prepare_question_verb(verb, sentence, includes_subject=True)
+#     verb_form_with_pobject = prepare_question_verb(verb, sentence, includes_subject=False)
+#
+#     if is_valid_subject(subj):
+#         question = [wh_word_pobject]
+#
+#         if len(verb_form_with_subject) == 1:
+#             question.append(verb_form_with_subject[0])
+#             question.append(format_phrase(subj))
+#         else:
+#             # I don't like this ... could have more than 2 in verb phrase, not in attribute, but other ones yes
+#             question.append(verb_form_with_subject[0])
+#             question.append(format_phrase(subj))
+#             question.extend(verb_form_with_subject[1:])
+#
+#         question = ' '.join(question)
+#         question += '?'
+#         questions.append(question)
+#
+#     question = [wh_word_subject]
+#     question.extend(verb_form_with_pobject)
+#     question.append(format_phrase(pobject_phrase))
+#
+#     question = ' '.join(question)
+#     question += '?'
+#     questions.append(question)
+
 def generate_attribute_questions(match):
     """Gets a match object for the attribute pattern. Returns a list of questions"""
     questions = []
@@ -144,11 +311,8 @@ def generate_attribute_questions(match):
     subject = match.get_first_token()
     verb = subject.head
 
-    subject_phrase = get_noun_phrase(subject, sentence)
-    attribute = get_noun_phrase(match.get_last_token(), sentence)
-
-    print(subject_phrase)
-    print(attribute)
+    subject_phrase = extract_noun_phrase(subject, sentence)
+    attribute = extract_noun_phrase(match.get_last_token(), sentence)
 
     wh_word_subject = choose_wh_word(subject_phrase)
     wh_word_attribute = choose_wh_word(attribute)
@@ -159,33 +323,76 @@ def generate_attribute_questions(match):
     if is_valid_subject(subject_phrase):
         questions.append('Describe or define ' + format_phrase(subject_phrase) + '.')
         question = [wh_word_attribute]
-        if len(verb_form_with_subject) > 2:
-            # Need to carefully think behaviour in this situation
-            return
 
-        elif len(verb_form_with_subject) == 1:
+        if len(verb_form_with_subject) == 1:
             question.append(verb_form_with_subject[0])
             question.append(format_phrase(subject_phrase))
         else:
             # I don't like this ... could have more than 2 in verb phrase, not in attribute, but other ones yes
             question.append(verb_form_with_subject[0])
             question.append(format_phrase(subject_phrase))
-            question.append(verb_form_with_subject[1])
+            question.extend(verb_form_with_subject[1:])
 
         question = ' '.join(question)
         question += '?'
         questions.append(question)
 
     question = [wh_word_subject]
-    if len(verb_form_with_attribute) > 2:
+    question.extend(verb_form_with_attribute)
+    question.append(format_phrase(attribute))
+
+    question = ' '.join(question)
+    question += '?'
+    questions.append(question)
+
+    return questions
+
+
+def generate_dobj_questions(match):
+    questions = []
+    sentence = match.get_sentence()
+    direct_object = match.get_last_token()
+    subject = match.get_first_token()
+    verb = subject.head
+
+    # Means we've matched in error
+    if direct_object.head != verb:
         return
-    elif len(verb_form_with_attribute) == 1:
-        question.append(verb_form_with_attribute[0])
-        question.append(format_phrase(attribute))
-    else:
-        question.append(verb_form_with_attribute[0])
-        question.append(format_phrase(attribute))
-        question.append(verb_form_with_attribute[1])
+
+    subject_phrase = extract_noun_phrase(subject, sentence)
+    direct_object_phrase = extract_noun_phrase(direct_object, sentence)
+
+    wh_word_subject = choose_wh_word(subject_phrase)
+    wh_word_object = choose_wh_word(direct_object_phrase)
+
+    verb_form_with_subject = prepare_question_verb(verb, sentence, includes_subject=True)
+    verb_form_with_object = prepare_question_verb(verb, sentence, includes_subject=False)
+
+    if is_valid_subject(subject_phrase):
+        questions.append('Describe the relation or interaction between '
+                         + format_phrase(subject_phrase)
+                         + ' and '
+                         + format_phrase(direct_object_phrase)
+                         + '.')
+        question = [wh_word_object]
+
+        if len(verb_form_with_subject) == 1:
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+        else:
+            # I don't like this ... could have more than 2 in verb phrase, not in attribute, but other ones yes
+            question.append(verb_form_with_subject[0])
+            question.append(format_phrase(subject_phrase))
+            question.extend(verb_form_with_subject[1:])
+
+        question = ' '.join(question)
+        question += '?'
+        questions.append(question)
+
+    question = [wh_word_subject]
+
+    question.extend(verb_form_with_object)
+    question.append(format_phrase(direct_object_phrase))
 
     question = ' '.join(question)
     question += '?'
@@ -230,14 +437,15 @@ def show_dependencies(sentence, port=5000):
     displacy.serve(sentence, style='dep', port=port)
 
 
-def get_noun_phrase(token, sentence):
+def extract_noun_phrase(token, sentence, exclude_span=None):
     start_index = INFINITY
     end_index = -1
 
     for child in token.subtree:
         """This will fail in some cases, might want to try to just get full subtree, but then need to pay attention 
         what we call it on. For now, I'm going to call it only on subjects and object so should be OK to get subtree"""
-
+        if child in exclude_span:
+            continue
         # if child.dep_.endswith("mod") \
         #         or child.dep_ == "compound" \
         #         or child == token \
@@ -278,7 +486,7 @@ def is_valid_sentence(sentence):
 
 def trial_sentences():
     text = NLP(u'Computer architecture is a set of rules that describe the functionality of computer systems.')
-    text_2 = NLP(u"Apple's logo was designed for Steve Jobs.")
+    text_2 = NLP(u"Apple's logo was designed by Steve Jobs.")
     text_3 = NLP(u"Stacy went to see Johnny at the store.")
     doc2 = NLP('John studied, hoping to get a good grade.')
     doc3 = NLP(u'I bought the book that inspired Bob.')
@@ -292,7 +500,9 @@ def trial_sentences():
     text_4 = NLP(u'A router is a networking device that forwards data packets between computer networks and a house is a living space.')
     d = NLP(u'A user centred design process provides a professional approach to creating software with functionality that users need.')
 
-    show_dependencies(d)
+    text = NLP(u"Apple's logo was designed by Steve Jobs in early december 2006 in front of the Empire State Building.")
+
+    show_dependencies(text)
     # for nc in text.noun_chunks:
     #     print(nc)
 
@@ -305,19 +515,52 @@ def is_past_tense(token):
 
 def generate_q():
     # text = NLP(u'Computer Science is the study of both practical and theoretical approaches to computers.')
-    text = NLP(u'A router is a networking device that forwards data packets between computer networks.')
+    # text = NLP(u'A router is a networking device that forwards data packets between computer networks.')
+    # text = NLP(u"Stacy went to see Johnny at the store.")
+    # text = NLP(u'From outside to inside, the chip contains several layers of complex intertwined transistors.')
+    # text = NLP(u'A user centred design process could provide a professional approach to creating software with functionality that users need.')
+    # text = NLP(u"Apple's logo was designed by Steve Jobs in early december 2006.")
+    # text = NLP(u'John gave Mary the book.')
+    # text = NLP(u"Apple's logo was designed by Steve Jobs in early december 2006.")
+    # text = NLP(u'A computer scientist specializes in the theory of computation.')
+    # text = NLP(u'Machines for calculating fixed numerical tasks such as the abacus have existed since antiquity.')
+    text = NLP(u'The ecclesiastical parish of Navenby was originally placed in the Longoboby Rural Deanery.')
+    all_questions = []
 
-    matches = ATTR(text)
+    for tok in text:
+        print(tok.ent_type_)
+    matches = MATCHER(text)
     for ent_id, start, end in matches:
         match = Match(ent_id, start, end, text)
-        print(generate_attribute_questions(match))
+        pattern_name = NLP.vocab.strings[ent_id]
+        # print(pattern_name)
+        questions = handle_match(pattern_name)(match)
+        if questions:
+            all_questions.extend(questions)
+
+    for question in all_questions:
+        print(question)
+
+
+# Switch statement, sort of
+pattern_to_question = {
+    "ATTR": lambda match: generate_attribute_questions(match),
+    "POBJ": lambda match: generate_prepositional_questions(match),
+    "DOBJ": lambda match: generate_dobj_questions(match),
+    "AGENT": lambda match: generate_agent_questions(match)
+}
+
+
+def handle_match(pattern_name):
+    return pattern_to_question[pattern_name]
 
 
 if __name__ == '__main__':
     # generate_questions(TEST_TEXT)
-    trial_sentences()
     # doc = NLP(u'Computer Science is the study of both practical and theoretical approaches to computers. A computer scientist specializes in the theory of computation.')
     # sentences = list(doc.sents)
     # show_dependencies(sentences[1].as_doc(), port=5001)
-    # initialize_patterns()
-    # generate_q()
+    initialize_patterns()
+    generate_q()
+    # trial_sentences()
+
