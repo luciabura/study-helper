ee11f0ff8df8c9b80d1dbea9c596220f48cd305f
Unknown <luciabura@gmail.com>
2018-01-19 22:40:36 +0000

    Question evaluation Initial commit

---
+++ evaluation/question_evaluation.py
@@ -0,0 +1,18 @@
+"""
+Takes qq system output
+Heilman QG output
+
+Measures:
+1) syntactical correctness
+2) Similarity with gold questions -> spacy similarity + words present + phrase sequence
+3*) relevance to topic if topic exists
+"""
+
+
+def similarity_overlap_score(q1, q2):
+    """Takes two questions as text"""
+    pass
+
+
+def semantic_overlap_score(s1, s2):
+    pass
