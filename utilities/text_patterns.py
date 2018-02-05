77e557eac1a8b017d52292c980880523462ddf68
Unknown <luciabura@gmail.com>
2018-02-05 23:16:22 +0000

    Add intention to test pattern distributions

---
+++ utilities/text_patterns.py
@@ -0,0 +1,9 @@
+def inspect_text_pattern_distribution(text):
+    """gets percentage for 5-6 patterns for a particular text"""
+    pass
+
+
+def aggregate_pattern_percentage(folder_path):
+    """Look into a folder and appply inspect all texts for patterns
+    Proceed to """
+    pass
