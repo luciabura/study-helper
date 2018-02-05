07294e7cc5c48c654d818f00a0150b751039db20
Unknown <luciabura@gmail.com>
2018-02-05 23:20:33 +0000

    Attempt at using perplexity

--- evaluation/question_evaluation.py
+++ evaluation/question_evaluation.py
@@ -9,6 +9,29 @@ Measures:
 """
 
 
+from sklearn.decomposition import LatentDirichletAllocation
+from text_processing.preprocessing import spacy_word_tokenize
+from utilities.read_write import read_file
+if __name__ == '__main__':
+    text = "What is the colour of his hair?"
+    text2 = "What are John doing?"
+    text3 = "What is John doing?"
+    lda = LatentDirichletAllocation()
+    t_tokens = spacy_word_tokenize(text)
+
+    corpus_filepath = input("Corpus filepath:")
+    corpus_text = read_file(corpus_filepath)
+    tokens = spacy_word_tokenize(corpus_text)
+
+    lda.fit(tokens)
+    p_s = lda.perplexity(t_tokens)
+    # p_s2 = LatentDirichletAllocation.perplexity(text2)
+    # p_s3 = LatentDirichletAllocation.perplexity(text3)
+    """Our perplexity for the second question should be higher"""
+    print(p_s)
+    # print(p_s2)
+    # print(p_s3)
+
 def similarity_overlap_score(q1, q2):
     """Takes two questions as text"""
     pass
