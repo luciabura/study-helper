862c45be5fe696e262b9fc62ce5ae26d9b3dd1b9
Unknown <luciabura@gmail.com>
2018-03-28 09:04:47 +0100

    Aggregate json information

--- utilities/json_file.py
+++ utilities/json_file.py
@@ -32,6 +32,25 @@ def get_text_with_questions():
     return text_with_questions
 
 
+def map_paragraphs_to_questions_json():
+    new_data = {"data": []}
+
+    data = data_file['data']
+    for data_el in data:
+        title = data_el['title']
+        for paragraph in data_el['paragraphs']:
+            text = paragraph['context']
+            questions = []
+            for qa in paragraph['qas']:
+                questions.append(qa['question'])
+
+            text_with_q = {'title': title, 'text': text, 'questions': questions}
+            new_data['data'].append(text_with_q)
+
+    with open('paragraphs_with_questions.json', 'w') as f_json:
+        json.dump(new_data, f_json)
+
+
 def print_all_questions():
     text_with_questions = get_text_with_questions()
     all_questions = []
@@ -43,7 +62,8 @@ def print_all_questions():
 
 
 if __name__ == '__main__':
-    print_all_questions()
+    # print_all_questions()
+    map_paragraphs_to_questions_json()
 
 
 
