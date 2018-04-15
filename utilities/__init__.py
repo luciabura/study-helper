762a9fb68b48429a35ee5806bd1a95bbf3390454
Unknown <luciabura@gmail.com>
2018-04-15 09:40:48 +0100

    Use md model
    Otherwise travis terminates...

--- utilities/__init__.py
+++ utilities/__init__.py
@@ -2,7 +2,7 @@ import spacy
 import math
 
 # Load spacy model
-# NLP = spacy.load('en_core_web_md')
-NLP = spacy.load('en_core_web_lg')
+NLP = spacy.load('en_core_web_md')
+# NLP = spacy.load('en_core_web_lg')
 
 INFINITY = math.inf
