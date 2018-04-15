e8ebf61ae8ef3c8787f64078745f320ceba461b6
Unknown <luciabura@gmail.com>
2018-04-15 12:09:02 +0100

    Delete unwanted files

--- runCoreNLPStanford.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
--- setupCLASSPATH.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-for file in `find ~/part-II/stanford-corenlp-full-2017-06-09/ -name "*.jar"`; do export
-CLASSPATH="$CLASSPATH:`realpath $file`"; done
